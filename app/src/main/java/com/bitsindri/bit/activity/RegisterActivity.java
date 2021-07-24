44a1d15f1f2b7cf3a9442f2dd8d3ef392a2d5465
Prafulla Shekhar <prafullashekhar@gmail.com>
2021-07-24 18:03:00 +0530

    validated the register process

--- app/src/main/java/com/bitsindri/bit/activity/RegisterActivity.java
+++ app/src/main/java/com/bitsindri/bit/activity/RegisterActivity.java
@@ -3,16 +3,15 @@ package com.bitsindri.bit.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bitsindri.bit.MainActivity;
 import com.bitsindri.bit.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -46,18 +45,25 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-    // function to add user to the firebase database
+    // function to validate and add user to the firebase database
     private void addUser(){
         String strEmail = email.getText().toString().trim();
         String strPassword = password.getText().toString();
 
+        // validating the user
         if(TextUtils.isEmpty(strEmail)){
             email.setError("Email cannot be empty");
             email.requestFocus();
         }else if(TextUtils.isEmpty(strPassword)){
             password.setError("Password cannot be empty");
             password.requestFocus();
-        }else{
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()){
+            email.setError("Please enter a valid email");
+            email.requestFocus();
+        }else if(strPassword.length() < 6){
+            password.setError("Too short password length");
+            password.requestFocus();
+        } else{
             // add the user
             mAuth.createUserWithEmailAndPassword(strEmail, strPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
@@ -73,9 +79,4 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    private void openMainActivity(){
-        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
 }
