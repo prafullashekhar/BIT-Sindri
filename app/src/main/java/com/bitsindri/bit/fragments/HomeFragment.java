ab6922f6bbe5089d1dd4ffbc65edcf0f1a7362ea
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2021-08-13 21:21:02 +0530

    added sliding images with samples (#11)
    * added sliding images with samples
    
    * moved sliding indicator to left bottom
    
    Co-authored-by: Prafulla Shekhar <77625900+prafullashekhar@users.noreply.github.com>

--- app/src/main/java/com/bitsindri/bit/fragments/HomeFragment.java
+++ app/src/main/java/com/bitsindri/bit/fragments/HomeFragment.java
@@ -11,18 +11,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bitsindri.bit.R;
+
+import com.bitsindri.bit.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 import com.bitsindri.bit.activity.HomeDepartmentActivity;
+
 
 
 public class HomeFragment extends Fragment {
 
+    SliderView sliderView;
+    int[] images = {R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,};
+
     public HomeFragment() {
         // Required empty public constructor
     }
+
     CardView departments;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+
+        sliderView =v.findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+      
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_home, container, false);
         departments=view.findViewById(R.id.home_departments);
@@ -33,5 +57,6 @@ public class HomeFragment extends Fragment {
             }
         });
         return view;
+
     }
 }
