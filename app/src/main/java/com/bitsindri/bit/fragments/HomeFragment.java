6578f3fda386c6180100107f2b8aa83890bf4928
prafullashekhar <prafullashekhar@gmail.com>
2021-08-15 01:36:02 +0530

    connected with realtime database and created image node

--- app/src/main/java/com/bitsindri/bit/fragments/HomeFragment.java
+++ app/src/main/java/com/bitsindri/bit/fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,11 @@ import com.bitsindri.bit.MainActivity;
 import com.bitsindri.bit.R;
 
 import com.bitsindri.bit.Adapter.SliderAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
@@ -27,6 +33,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     SliderView sliderView;
+    DatabaseReference SliderTopReference;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -44,17 +51,34 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
 
+        // initiating realtime database
+        SliderTopReference = FirebaseDatabase.getInstance().getReference().child("SlidingImage").child("HomeTop");
+
         sliderView =view.findViewById(R.id.image_slider);
 
-
-
+        // sliding image list
         List<String> images = new ArrayList<>();
-        images.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg");
-        images.add("https://thumbs.dreamstime.com/z/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg");
-        images.add("https://www.oberlo.com/media/1603969791-image-1.jpg?fit=max&fm=webp&w=1824");
-
         SliderAdapter sliderAdapter = new SliderAdapter(getContext());
-        sliderAdapter.renewItems(images);
+
+        SliderTopReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    images.clear();
+                    for(DataSnapshot dss : snapshot.getChildren()){
+                        String imgUrl = dss.getValue(String.class);
+                        images.add(imgUrl);
+                    }
+
+                    sliderAdapter.renewItems(images);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                sliderAdapter.renewItems(images);
+            }
+        });
 
         sliderView.setSliderAdapter(sliderAdapter);
         sliderView.setAutoCycle(true);
