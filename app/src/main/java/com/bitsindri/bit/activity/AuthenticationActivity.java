7f790a8a785bef87471d8bdb5f2b972e60677ad8
Nipun <nipunvirat0@gmail.com>
2021-08-20 15:29:28 +0530

    added animation

--- app/src/main/java/com/bitsindri/bit/activity/AuthenticationActivity.java
+++ app/src/main/java/com/bitsindri/bit/activity/AuthenticationActivity.java
@@ -1,53 +1,33 @@
 package com.bitsindri.bit.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 
 import com.bitsindri.bit.MainActivity;
 import com.bitsindri.bit.R;
 import com.bitsindri.bit.authenticationfrag.LoginFragment;
 import com.bitsindri.bit.authenticationfrag.RegisterFragment;
 import com.bitsindri.bit.authenticationfrag.SignUpButtonClickListener;
-import com.bitsindri.bit.methods.Constants;
-import com.bitsindri.bit.methods.Methods;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.jetbrains.annotations.NotNull;
 
 public class AuthenticationActivity extends AppCompatActivity implements SignUpButtonClickListener {
 
     private FirebaseAuth mAuth;
     LoginFragment fragment;
     private boolean isRegisterActive = false;
+    private FrameLayout container;
 
     @Override
     public void onBackPressed() {
         if(isRegisterActive){
             getSupportFragmentManager().beginTransaction().replace(R.id.authentication_container,fragment).commit();
+            container.setAnimation(AnimationUtils.loadAnimation(AuthenticationActivity.this,R.anim.up_to_down));
             isRegisterActive = false;
         }
         else super.onBackPressed();
@@ -57,6 +37,8 @@ public class AuthenticationActivity extends AppCompatActivity implements SignUpB
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        container= findViewById(R.id.authentication_container);
         mAuth = FirebaseAuth.getInstance();
         fragment = new LoginFragment();
         fragment.setSignUpClickListener(this);
@@ -80,6 +62,7 @@ public class AuthenticationActivity extends AppCompatActivity implements SignUpB
         if(view.getId() == R.id.signUp) {
             RegisterFragment registerFragment = new RegisterFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.authentication_container, registerFragment).commit();
+            container.setAnimation(AnimationUtils.loadAnimation(AuthenticationActivity.this,R.anim.down_to_up));
             isRegisterActive = true;
         }
     }
