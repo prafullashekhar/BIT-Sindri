fc84636be642513a97ba931df7454ec62773f742
prafullashekhar <prafullashekhar@gmail.com>
2021-09-15 13:04:09 +0530

    made firebase operation in background thread

--- app/src/main/java/com/bitsindri/bit/Repository/ProfileSharedPreferencesRepository.java
+++ app/src/main/java/com/bitsindri/bit/Repository/ProfileSharedPreferencesRepository.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
@@ -39,8 +40,10 @@ public class ProfileSharedPreferencesRepository {
     private final SharedPreferences sharedPreference;
     private String profileDownloadUrl;
 
+    @SuppressLint("StaticFieldLeak")
     private static volatile ProfileSharedPreferencesRepository INSTANCE;
-    Context context;
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
 
     public static ProfileSharedPreferencesRepository getInstance(Application application){
         if(INSTANCE == null){
@@ -50,13 +53,13 @@ public class ProfileSharedPreferencesRepository {
     }
 
     public ProfileSharedPreferencesRepository(Application application){
-        this.context = application;
+        context = application;
         sharedPreference = application.getSharedPreferences(Constants.SHARED_PREF_FILE, Context.MODE_PRIVATE);
     }
 
     public MutableLiveData<User> getUser(){
         if(sharedPreference.getString(Constants.NAME, "").equals(""))
-            loadData();
+            new getUserAsyncTask().execute();
 
         User user = new User(
                 sharedPreference.getString(Constants.NAME, ""),
@@ -103,92 +106,93 @@ public class ProfileSharedPreferencesRepository {
         editor.putString(Constants.ABOUT, updatedUser.getAbout());
         editor.apply();
 
-        uploadData(updatedUser);
+//        uploadData(updatedUser);
+        new uploadUserAsyncTask().execute(updatedUser);
     }
 
     // functions to load and upload data from fire store database
-    private void loadData() {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = auth.getCurrentUser();
-        assert currentUser != null;
+//    private void loadData() {
+//        FirebaseAuth auth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = auth.getCurrentUser();
+//        assert currentUser != null;
+//
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        DocumentReference reference = db.collection("Users").document(currentUser.getUid());
+//
+//        reference.get()
+//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        if(documentSnapshot.exists()){
+//                            SharedPreferences.Editor editor = sharedPreference.edit();
+//                            editor.putString(Constants.NAME, documentSnapshot.getString(Constants.NAME));
+//                            editor.putString(Constants.BATCH, documentSnapshot.getString(Constants.BATCH));
+//                            editor.putString(Constants.EMAIL, documentSnapshot.getString(Constants.EMAIL));
+//                            editor.putString(Constants.BRANCH, documentSnapshot.getString(Constants.BRANCH));
+//                            editor.putString(Constants.DOB, documentSnapshot.getString(Constants.DOB));
+//                            editor.putString(Constants.ROLL, documentSnapshot.getString(Constants.ROLL));
+//                            editor.putString(Constants.REG, documentSnapshot.getString(Constants.REG));
+//                            editor.putString(Constants.PROFILE_PIC, documentSnapshot.getString(Constants.PROFILE_PIC));
+//                            editor.putString(Constants.CLUB, documentSnapshot.getString(Constants.CLUB));
+//                            editor.putString(Constants.CODECHEF, documentSnapshot.getString(Constants.CODECHEF));
+//                            editor.putString(Constants.CODEFORCES, documentSnapshot.getString(Constants.CODEFORCES));
+//                            editor.putString(Constants.GITHUB, documentSnapshot.getString(Constants.GITHUB));
+//                            editor.putString(Constants.LINKEDIN, documentSnapshot.getString(Constants.LINKEDIN));
+//                            editor.putString(Constants.FACEBOOK, documentSnapshot.getString(Constants.FACEBOOK));
+//                            editor.putString(Constants.INSTAGRAM, documentSnapshot.getString(Constants.INSTAGRAM));
+//                            editor.putString(Constants.ABOUT, documentSnapshot.getString(Constants.ABOUT));
+//
+//                            editor.apply();
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.e(Constants.msg, "Unable to get user data "+e.toString());
+//            }
+//        });
+//
+//    }
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference reference = db.collection("Users").document(currentUser.getUid());
-
-        reference.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if(documentSnapshot.exists()){
-                            SharedPreferences.Editor editor = sharedPreference.edit();
-                            editor.putString(Constants.NAME, documentSnapshot.getString(Constants.NAME));
-                            editor.putString(Constants.BATCH, documentSnapshot.getString(Constants.BATCH));
-                            editor.putString(Constants.EMAIL, documentSnapshot.getString(Constants.EMAIL));
-                            editor.putString(Constants.BRANCH, documentSnapshot.getString(Constants.BRANCH));
-                            editor.putString(Constants.DOB, documentSnapshot.getString(Constants.DOB));
-                            editor.putString(Constants.ROLL, documentSnapshot.getString(Constants.ROLL));
-                            editor.putString(Constants.REG, documentSnapshot.getString(Constants.REG));
-                            editor.putString(Constants.PROFILE_PIC, documentSnapshot.getString(Constants.PROFILE_PIC));
-                            editor.putString(Constants.CLUB, documentSnapshot.getString(Constants.CLUB));
-                            editor.putString(Constants.CODECHEF, documentSnapshot.getString(Constants.CODECHEF));
-                            editor.putString(Constants.CODEFORCES, documentSnapshot.getString(Constants.CODEFORCES));
-                            editor.putString(Constants.GITHUB, documentSnapshot.getString(Constants.GITHUB));
-                            editor.putString(Constants.LINKEDIN, documentSnapshot.getString(Constants.LINKEDIN));
-                            editor.putString(Constants.FACEBOOK, documentSnapshot.getString(Constants.FACEBOOK));
-                            editor.putString(Constants.INSTAGRAM, documentSnapshot.getString(Constants.INSTAGRAM));
-                            editor.putString(Constants.ABOUT, documentSnapshot.getString(Constants.ABOUT));
-
-                            editor.apply();
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.e(Constants.msg, "Unable to get user data "+e.toString());
-            }
-        });
-
-    }
-
-    private void uploadData(User updatedUser) {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = auth.getCurrentUser();
-        assert currentUser != null;
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference reference = db.collection("Users").document(currentUser.getUid());
-
-        Map<String, Object> map = new HashMap<>();
-        map.put(Constants.NAME, updatedUser.getName());
-        map.put(Constants.EMAIL, updatedUser.getEmail());
-        map.put(Constants.BATCH, updatedUser.getBatch());
-        map.put(Constants.BRANCH, updatedUser.getBranch());
-        map.put(Constants.ROLL, updatedUser.getRollNo());
-        map.put(Constants.REG, updatedUser.getRegNo());
-        map.put(Constants.DOB, updatedUser.getDob());
-        map.put(Constants.PROFILE_PIC, updatedUser.getProfilePic());
-        map.put(Constants.CLUB, updatedUser.getClub());
-        map.put(Constants.CODECHEF, updatedUser.getCodechefUrl());
-        map.put(Constants.CODEFORCES, updatedUser.getCodefrocesUrl());
-        map.put(Constants.GITHUB, updatedUser.getGithubUrl());
-        map.put(Constants.LINKEDIN, updatedUser.getLinkedInUrl());
-        map.put(Constants.FACEBOOK, updatedUser.getFacebookUrl());
-        map.put(Constants.INSTAGRAM, updatedUser.getInstaUrl());
-        map.put(Constants.ABOUT, updatedUser.getAbout());
-
-        reference.set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                TastyToast.makeText(context, "Updated Successfully", Toast.LENGTH_SHORT, TastyToast.SUCCESS);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.e(Constants.msg, "unable to update user");
-            }
-        });
-
-    }
+//    private void uploadData(User updatedUser) {
+//        FirebaseAuth auth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = auth.getCurrentUser();
+//        assert currentUser != null;
+//
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        DocumentReference reference = db.collection("Users").document(currentUser.getUid());
+//
+//        Map<String, Object> map = new HashMap<>();
+//        map.put(Constants.NAME, updatedUser.getName());
+//        map.put(Constants.EMAIL, updatedUser.getEmail());
+//        map.put(Constants.BATCH, updatedUser.getBatch());
+//        map.put(Constants.BRANCH, updatedUser.getBranch());
+//        map.put(Constants.ROLL, updatedUser.getRollNo());
+//        map.put(Constants.REG, updatedUser.getRegNo());
+//        map.put(Constants.DOB, updatedUser.getDob());
+//        map.put(Constants.PROFILE_PIC, updatedUser.getProfilePic());
+//        map.put(Constants.CLUB, updatedUser.getClub());
+//        map.put(Constants.CODECHEF, updatedUser.getCodechefUrl());
+//        map.put(Constants.CODEFORCES, updatedUser.getCodefrocesUrl());
+//        map.put(Constants.GITHUB, updatedUser.getGithubUrl());
+//        map.put(Constants.LINKEDIN, updatedUser.getLinkedInUrl());
+//        map.put(Constants.FACEBOOK, updatedUser.getFacebookUrl());
+//        map.put(Constants.INSTAGRAM, updatedUser.getInstaUrl());
+//        map.put(Constants.ABOUT, updatedUser.getAbout());
+//
+//        reference.set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void unused) {
+//                TastyToast.makeText(context, "Updated Successfully", Toast.LENGTH_SHORT, TastyToast.SUCCESS);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.e(Constants.msg, "unable to update user");
+//            }
+//        });
+//
+//    }
     public void uploadProfilePicInStorage(Uri imageToBeUpload , ImageView img , ProgressBar progressBar){
         img.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         progressBar.setVisibility(View.VISIBLE);
@@ -230,5 +234,112 @@ public class ProfileSharedPreferencesRepository {
         });
     }
 
+    /* ------------------------------------- async task to ger user data ---------------------------------------------------*/
+    private static class getUserAsyncTask extends AsyncTask<Void, Void, Void>
+    {
+        DocumentReference reference;
+        SharedPreferences sharedPreferences;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            FirebaseUser currentUser = auth.getCurrentUser();
+            assert currentUser != null;
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            reference = db.collection("Users").document(currentUser.getUid());
+
+            sharedPreferences = context.getSharedPreferences(Constants.SHARED_PREF_FILE, Context.MODE_PRIVATE);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            reference.get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if(documentSnapshot.exists()){
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(Constants.NAME, documentSnapshot.getString(Constants.NAME));
+                                editor.putString(Constants.BATCH, documentSnapshot.getString(Constants.BATCH));
+                                editor.putString(Constants.EMAIL, documentSnapshot.getString(Constants.EMAIL));
+                                editor.putString(Constants.BRANCH, documentSnapshot.getString(Constants.BRANCH));
+                                editor.putString(Constants.DOB, documentSnapshot.getString(Constants.DOB));
+                                editor.putString(Constants.ROLL, documentSnapshot.getString(Constants.ROLL));
+                                editor.putString(Constants.REG, documentSnapshot.getString(Constants.REG));
+                                editor.putString(Constants.PROFILE_PIC, documentSnapshot.getString(Constants.PROFILE_PIC));
+                                editor.putString(Constants.CLUB, documentSnapshot.getString(Constants.CLUB));
+                                editor.putString(Constants.CODECHEF, documentSnapshot.getString(Constants.CODECHEF));
+                                editor.putString(Constants.CODEFORCES, documentSnapshot.getString(Constants.CODEFORCES));
+                                editor.putString(Constants.GITHUB, documentSnapshot.getString(Constants.GITHUB));
+                                editor.putString(Constants.LINKEDIN, documentSnapshot.getString(Constants.LINKEDIN));
+                                editor.putString(Constants.FACEBOOK, documentSnapshot.getString(Constants.FACEBOOK));
+                                editor.putString(Constants.INSTAGRAM, documentSnapshot.getString(Constants.INSTAGRAM));
+                                editor.putString(Constants.ABOUT, documentSnapshot.getString(Constants.ABOUT));
+
+                                editor.apply();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(Constants.msg, "Unable to get user data "+e.toString());
+                }
+            });
+            return null;
+        }
+    }
+
+    /* --------------------------------- async task to update user data in fire store ---------------------------------------------*/
+    private static class uploadUserAsyncTask extends AsyncTask<User, Void, Void>
+    {
+        DocumentReference reference;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            FirebaseUser currentUser = auth.getCurrentUser();
+            assert currentUser != null;
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            reference = db.collection("Users").document(currentUser.getUid());
+        }
+
+        @Override
+        protected Void doInBackground(User... users) {
+            User updatedUser = users[0];
+            Map<String, Object> map = new HashMap<>();
+            map.put(Constants.NAME, updatedUser.getName());
+            map.put(Constants.EMAIL, updatedUser.getEmail());
+            map.put(Constants.BATCH, updatedUser.getBatch());
+            map.put(Constants.BRANCH, updatedUser.getBranch());
+            map.put(Constants.ROLL, updatedUser.getRollNo());
+            map.put(Constants.REG, updatedUser.getRegNo());
+            map.put(Constants.DOB, updatedUser.getDob());
+            map.put(Constants.PROFILE_PIC, updatedUser.getProfilePic());
+            map.put(Constants.CLUB, updatedUser.getClub());
+            map.put(Constants.CODECHEF, updatedUser.getCodechefUrl());
+            map.put(Constants.CODEFORCES, updatedUser.getCodefrocesUrl());
+            map.put(Constants.GITHUB, updatedUser.getGithubUrl());
+            map.put(Constants.LINKEDIN, updatedUser.getLinkedInUrl());
+            map.put(Constants.FACEBOOK, updatedUser.getFacebookUrl());
+            map.put(Constants.INSTAGRAM, updatedUser.getInstaUrl());
+            map.put(Constants.ABOUT, updatedUser.getAbout());
+
+            reference.set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    TastyToast.makeText(context, "Updated Successfully", Toast.LENGTH_SHORT, TastyToast.SUCCESS);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(Constants.msg, "unable to update user");
+                }
+            });
+
+            return null;
+        }
+    }
 
 }
